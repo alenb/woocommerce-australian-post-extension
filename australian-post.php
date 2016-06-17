b544bc7708303626da725df97b37c1a53d81c3bf
Waseem Senjer <waseem.senjer@gmail.com>
2016-06-18 01:47:47 +0300

    support legacy methods in less than WC 2.6

--- australian-post.php
+++ australian-post.php
@@ -26,13 +26,20 @@ if(auspost_is_auspost_pro_active()){
 if(auspost_is_woocommerce_active()){
 		add_filter('woocommerce_shipping_methods', 'add_australian_post_method');
 		function add_australian_post_method( $methods ){
-			$methods['auspost'] = 'WC_Australian_Post_Shipping_Method';
+			if(version_compare(WC()->version, '2.6.0', 'lt')){
+				$methods['auspost'] = 'WC_Australian_Post_Shipping_Method_Legacy';
+			}else{
+				$methods['auspost'] = 'WC_Australian_Post_Shipping_Method';
+			}
 			return $methods; 
 		}
 
 		add_action('woocommerce_shipping_init', 'init_australian_post');
 		function init_australian_post( ){
 			require 'class-australian-post.php';
+			if(version_compare(WC()->version, '2.6.0', 'lt')){
+				require 'class-australian-post-legacy.php';
+			}
 		}
 }
 
--- class-australian-post-legacy.php
+++ class-australian-post-legacy.php
@@ -1,28 +1,15 @@
 <?php 
 
-class WC_Australian_Post_Shipping_Method extends WC_Shipping_Method{
+class WC_Australian_Post_Shipping_Method_Legacy extends WC_Australian_Post_Shipping_Method{
 
 
 
-	public $postageParcelURL = 'https://digitalapi.auspost.com.au/postage/parcel/domestic/calculate.json';
-	public $postage_intl_url = 'https://digitalapi.auspost.com.au/postage/parcel/international/service.json';
-	public $api_key = '20b5d076-5948-448f-9be4-f2fd20d4c258';
-	public $supported_services = array( 
-		'AUS_PARCEL_REGULAR' => 'Parcel Post',
-		'AUS_PARCEL_EXPRESS' => 'Express Post'
-	);
-	
-	public function __construct( $instance_id = 0 ){
+	public function __construct(){
+		parent::__construct(0);
 		$this->id = 'auspost';
-		$this->instance_id = absint( $instance_id );
 		$this->method_title = __('Australia Post','australian-post');
 		$this->title = __('Australia Post','australian-post');
 		
-		$this->supports  = array(
-			'shipping-zones',
-			'instance-settings',
-			'instance-settings-modal',
-		);
 		$this->init_form_fields();
 		$this->init_settings();
 
@@ -45,9 +32,6 @@ class WC_Australian_Post_Shipping_Method extends WC_Shipping_Method{
 		
 		add_action('woocommerce_update_options_shipping_'.$this->id, array($this, 'process_admin_options'));
 
-
-
-
 	}
 
 
@@ -56,7 +40,7 @@ class WC_Australian_Post_Shipping_Method extends WC_Shipping_Method{
 		$dimensions_unit = strtolower( get_option( 'woocommerce_dimension_unit' ) );
 		$weight_unit = strtolower( get_option( 'woocommerce_weight_unit' ) );
 		
-		$this->instance_form_fields = array(
+		$this->form_fields = array(
 			'title' => array(
 				'title' 		=> __( 'Method Title', 'woocommerce' ),
 				'type' 			=> 'text',
@@ -141,7 +125,7 @@ class WC_Australian_Post_Shipping_Method extends WC_Shipping_Method{
 				<div id="post-body" class="metabox-holder columns-2">
 					<div id="post-body-content">
 						<table class="form-table">
-							<?php echo $this->get_admin_options_html();?>
+							<?php $this->generate_settings_html(); ?>
 						</table><!--/.form-table-->
 					</div>
 					<div id="postbox-container-1" class="postbox-container">
@@ -230,227 +214,6 @@ class WC_Australian_Post_Shipping_Method extends WC_Shipping_Method{
 					}
 				</style>
 		<?php
-	}
-
-	public function is_available( $package ){
-		// The lite version doesn't support international shipping
-		if($package['destination']['country'] != 'AU') return false;
-
-		// Debug mode
-		if($this->debug_mode === 'yes'){
-			return current_user_can('administrator');
-		}
-		return true;
-	}
-
-	public function calculate_shipping( $package = array() ){
-		$package_details  =  $this->get_package_details( $package );
-		$this->rates = array();	
-		// since 1.4.2 enhancing the debug mode.
-		$this->debug('Packing Details: <pre>' . print_r($package_details, true) . '</pre>');
-		$weight = 0;
-		$length = 0;
-		$width = 0;
-		$height = 0;
-		$rates = array();
-		foreach($package_details as  $pack){
-
-			$weight = $pack['weight'];
-			$height = $pack['height'];
-			$width 	= $pack['width'];
-			$length = $pack['length'];
-
-
-			$rates = $this->get_rates($rates, $pack['item_id'], $weight, $height, $width, $length, $package['destination']['postcode'] );
-			
-		}
-		
-		if(!empty($rates)){
-			foreach ($rates as $key => $rate) {
-				$rate['package'] = $package;
-				$this->add_rate($rate);
-			}
-		}
-		
-
-	}
-
-
-
-
-	private function get_rates( $old_rates, $item_id, $weight, $height, $width, $length, $destination ){
-
-		$query_params['from_postcode'] = $this->shop_post_code;
-		$query_params['to_postcode'] = $destination;
-		$query_params['length'] = $length;
-		$query_params['width'] = $width;
-		$query_params['height'] = $height;
-		$query_params['weight'] = $weight;
-		foreach($this->supported_services as $service_key => $service_name):
-					$query_params['service_code'] = $service_key;
-					$this->debug('Packing Request: <pre>' . print_r($this->postageParcelURL.'?'.http_build_query($query_params), true) . '</pre>');
-					
-					$response = wp_remote_get( $this->postageParcelURL.'?'.http_build_query($query_params),array('headers' => array('AUTH-KEY'=> $this->api_key)));
-					// since 1.4.2 enhancing the debug mode.
-					$this->debug('Australia Post RESPONSE: <pre>' . print_r($response, true) . '</pre>');
-					
-					if(is_wp_error( $response )){
-						return array('error' => 'Unknown Problem. Please Contact the admin');		
-					}
-
-					$aus_response = json_decode(wp_remote_retrieve_body($response));
-					
-
-					
-					if(!isset($aus_response->error) && $aus_response != null){
-						$duration = '';
-						if($this->show_duration === 'yes'){
-							$duration = ' ('. $aus_response->postage_result->delivery_time .')';
-						}
-
-						$old_rate = (isset($old_rates[$service_key]['cost']))?$old_rates[$service_key]['cost']:0;
-						// add the rate if the API request succeeded
-						
-						$rates[$service_key] = array(
-								'id' => $service_key,
-								'label' => $this->title. ' ' . $aus_response->postage_result->service.' ' . $duration,
-								'cost' =>  ($aus_response->postage_result->total_cost ) + $old_rate, 
-							
-						);
-						 
-					// if the API returned any error, show it to the user	
-					}else{
-						return array('error' => $aus_response->error->errorMessage);
- 
-					}
-					
-			endforeach;
-
-			return $rates;
-	}
-
-
-	/**
-	 * get_min_dimension function.
-	 * get the minimum dimension of the package, so we multiply it with the quantity
-	 * @access private
-	 * @param number $width
-	 * @param number $length
-	 * @param number $height
-	 * @return string $result
-	 */
-	private function get_min_dimension($width, $length, $height){
-
-		$dimensions = array('width'=>$width,'length'=>$length,'height'=>$height);
-		$result = array_keys($dimensions, min($dimensions));
-		return $result[0];
-	}
-
-
-/**
-     * get_package_details function.
-     *
-     * @access private
-     * @param mixed $package
-     * @return void
-     */
-    private function get_package_details( $package ) {
-	    global $woocommerce;
-
-	    $parcel   = array();
-	    $requests = array();
-    	$weight   = 0;
-    	$volume   = 0;
-    	$value    = 0;
-    	$products = array();
-    	// Get weight of order
-    	foreach ( $package['contents'] as $item_id => $values ) {
-
-    		$weight += woocommerce_get_weight( (floatval($values['data']->get_weight())<=0  )?$this->default_weight:$values['data']->get_weight(), 'kg' ) * $values['quantity'];
-    		$value  += $values['data']->get_price() * $values['quantity'];
-    		
-    		$length = woocommerce_get_dimension( ($values['data']->length=='')?$this->default_length:$values['data']->length, 'cm' );
-    		$height = woocommerce_get_dimension( ($values['data']->height=='')?$this->default_height:$values['data']->height, 'cm' );
-    		$width = woocommerce_get_dimension( ($values['data']->width=='')?$this->default_width:$values['data']->width, 'cm' );
-    		$min_dimension = $this->get_min_dimension( $width, $length, $height );
-    		$products[] = array('weight'=> woocommerce_get_weight( (floatval($values['data']->get_weight())<=0  )?$this->default_weight:$values['data']->get_weight(), 'kg' ),
-    							'quantity'=> $values['quantity'],
-    							'length'=> $length,
-    							'height'=> $height,
-    							'width'=> $width,
-    							'item_id'=> $item_id,
-    						);
-    		$volume += ( $length * $height * $width );
-    	}
-
-    	$max_weight = $this->get_max_weight($package);
-    	
-	    	$pack = array();
-			$packs_count = 1;
-			$pack[$packs_count]['weight'] = 0;
-			$pack[$packs_count]['length'] = 0;
-			$pack[$packs_count]['height'] = 0;
-			$pack[$packs_count]['width'] = 0;
-			//$pack[$packs_count]['quantity'] = 0;
-			foreach ($products as $product){
-				while ($product['quantity'] != 0) {
-					if(!isset($pack[$packs_count]['weight'])){
-						$pack[$packs_count]['weight'] = 0;
-					}
-					$pack[$packs_count]['weight'] += $product['weight'];
-					$pack[$packs_count]['length'] = $product['length'];
-					$pack[$packs_count]['height'] = $product['height'];
-					$pack[$packs_count]['width']  = $product['width'];
-					$pack[$packs_count]['item_id'] =  $product['item_id'];
-
-					if($pack[$packs_count]['weight'] > $max_weight ){
-						$pack[$packs_count]['weight'] -=  $product['weight'];
-						$packs_count++;
-						$pack[$packs_count]['weight'] = $product['weight'];
-						$pack[$packs_count]['length'] =1;// $product['length'];
-						$pack[$packs_count]['height'] =1;// $product['height'];
-						$pack[$packs_count]['width'] =1;// $product['width'];
-						$pack[$packs_count]['item_id'] =  $product['item_id'];
-					
-					}
-					$product['quantity']--;
-				}
-			}
-			
-    	return $pack;
-    }
-
-
-
-    private function get_max_weight( $package){
-    	$max = ( $package['destination']['country'] == 'AU' )? 22:20;
-    	$store_unit = strtolower( get_option('woocommerce_weight_unit') );
-    	
-    	if($store_unit == 'kg')
-    		return $max;
-    	if($store_unit == 'g')
-    		return $max * 1000;
-    	if($store_unit == 'lbs')
-    		return $max * 0.453592;
-    	if($store_unit == 'oz')
-    		return $max * 0.0283495;
-
-    	return $max;
-  
-    }
-
-	/**
-	 * Output a message
-	 */
-	public function debug($message, $type = 'notice') {
-		if ($this->debug_mode == 'yes' && current_user_can('manage_options')) {
-			if (version_compare(WOOCOMMERCE_VERSION, '2.1', '>=')) {
-				wc_add_notice($message, $type);
-			} else {
-				global $woocommerce;
-				$woocommerce->add_message($message);
-			}
-		}
 	}
 
 }
